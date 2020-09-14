fe2881530e68b445d174dcdf028e95e1db8aba72
Monsur26 <monsur.ahmed26@gmail.com>
2020-09-13 23:01:24 -0400

    updated

--- ChaseBank/src/main/java/webelements/HomePageWebElements.java
+++ ChaseBank/src/main/java/webelements/HomePageWebElements.java
@@ -3,4 +3,6 @@ package webelements;
 public class HomePageWebElements {
     public static final String sideMenu="skip-sidemenu";
     public static final String accountHover="";
+    public static final String fbIcon="//a[contains(@class,'regular-link chaseanalytics-opt-exlnk icon icon-facebook')]";
+    public static final String chaseIcon="//span[@class='chase-logo-icon']";
 }
---
+++ Jquery/src/main/java/webelements/HomePageWebElements.java
@@ -0,0 +1,7 @@
+package webelements;
+
+public class HomePageWebElements {
+    public static final String draggableMenu="//a[contains(text(),'Draggable')]";
+    public static final String dragItem="draggable";
+    public static final String datePickerS="//form//input[@name='bdaytime']";
+}
